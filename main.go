2d642d50fad0febfe9a8d61f1212bb08bcf2113d
mohit <mohit.gupta@clever.com>
2017-03-01 02:05:20 +0000

    basic make generate with swagger config

--- main.go
+++ main.go
@@ -3,20 +3,20 @@ package main
 import (
 	"flag"
 	"log"
-	"os"
 
 	"github.com/Clever/workflow-manager/gen-go/server"
 )
-
-// MyController implements server.Controller
-type MyController struct{}
 
 func main() {
 	addr := flag.String("addr", ":8080", "Address to listen at")
 	flag.Parse()
 
-	myController := MyController{}
-	s := server.New(myController, *addr)
-	// Serve should not return
-	log.Fatal(s.Serve())
+	wm := WorkflowManager{}
+	s := server.New(wm, *addr)
+
+	if err := s.Serve(); err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("workflow-manager exited without error")
 }
