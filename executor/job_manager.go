b4a631e8f5ea0a1ed99831e4fa3e27d71ea4fbb5
Nathan Leiby <nathanleiby@users.noreply.github.com>
2017-05-19 16:13:17 -0700

    executor: log job create, cancel, and status change (#13)
    * executor: log job create, cancel, and status change
    
    Also modifies the job status change logic, so that we only save the job
    to the store if its status has changed.
    
    * batch_test: use UTC times
    
    This was failing on my dev machine, since it's in PDT
    
    * (fix) s/job-id/id
    
    * executor: (revert) always update job in store
    
    tasks may have had status changes, even if overall job status has not
    changed
    
    * executor: log workflow name and version

--- executor/job_manager.go
+++ executor/job_manager.go
@@ -35,6 +35,8 @@ func NewBatchJobManager(executor Executor, store store.Store) BatchJobManager {
 
 // UpdateJobStatus ensures that the status of the tasks is in-sync with AWS Batch and sets Job status
 func (jm BatchJobManager) UpdateJobStatus(job *resources.Job) error {
+	previousStatus := job.Status
+
 	// copy current status
 	taskStatus := map[string]resources.TaskStatus{}
 	for _, task := range job.Tasks {
@@ -89,12 +91,27 @@ func (jm BatchJobManager) UpdateJobStatus(job *resources.Job) error {
 		job.Status = resources.Succeeded
 	}
 
+	// If the status was updated, log it and save to datastore
+	if previousStatus != job.Status {
+		log.InfoD("job-status-change", logger.M{
+			"id":               job.ID,
+			"workflow":         job.Workflow.Name(),
+			"workflow-version": job.Workflow.Version(),
+			"before":           previousStatus,
+			"after":            job.Status,
+		})
+	}
 	return jm.store.UpdateJob(*job)
 }
 
 // CreateJob can be used to create a new job for a workflow
 func (jm BatchJobManager) CreateJob(def resources.WorkflowDefinition, input []string) (*resources.Job, error) {
 	job := resources.NewJob(def, input)
+	log.InfoD("job-created", logger.M{
+		"id":               job.ID,
+		"workflow":         job.Workflow.Name(),
+		"workflow-version": job.Workflow.Version(),
+	})
 
 	err := jm.scheduleTasks(job, input)
 	if err != nil {
@@ -139,6 +156,13 @@ func (jm BatchJobManager) pollUpdateStatus(job *resources.Job) {
 
 func (jm BatchJobManager) CancelJob(job *resources.Job, reason string) error {
 	// don't cancel already succeeded tasks
+	log.InfoD("job-cancelled", logger.M{
+		"id":               job.ID,
+		"workflow":         job.Workflow.Name(),
+		"workflow-version": job.Workflow.Version(),
+		"reason":           reason,
+	})
+
 	tasks := []*resources.Task{}
 	for _, task := range job.Tasks {
 		switch task.Status {
