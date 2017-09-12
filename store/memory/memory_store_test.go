face8148e8adb1638af26f64a4bf3b86d7a85156
Nathan Leiby <nathan.leiby@clever.com>
2017-09-12 11:14:24 -0700

    store: refactor to keep tests in sync
    Previously, it would have been easy to run different tests against the
    memory store vs the dynamodb store, if you updated one but forgot to
    update the other.

--- store/memory/memory_store_test.go
+++ store/memory/memory_store_test.go
@@ -10,15 +10,6 @@ func TestMemoryStore(t *testing.T) {
 	s := New()
 	// GetWorkflowDefinitions does a primitive length check of the number of workflows received
 	// We should run it first for it to pass
-	t.Run("GetWorkflowDefinitions", tests.GetWorkflowDefinitions(s, t))
-	t.Run("UpdateWorkflowDefinition", tests.UpdateWorkflowDefinition(s, t))
-	t.Run("GetWorkflowDefinition", tests.GetWorkflowDefinition(s, t))
-	t.Run("SaveWorkflowDefinition", tests.SaveWorkflowDefinition(s, t))
-	t.Run("SaveStateResource", tests.SaveStateResource(s, t))
-	t.Run("GetStateResource", tests.GetStateResource(s, t))
-	t.Run("DeleteStateResource", tests.DeleteStateResource(s, t))
-	t.Run("SaveWorkflow", tests.SaveWorkflow(s, t))
-	t.Run("UpdateWorkflow", tests.UpdateWorkflow(s, t))
-	t.Run("GetWorkflowByID", tests.GetWorkflowByID(s, t))
-	t.Run("GetWorkflows", tests.GetWorkflows(s, t))
+
+	tests.RunStoreTests(s, t)
 }
