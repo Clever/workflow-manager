34462ebdddd9d082254a3d3940af1ebe8cf766c9
Rafael Garcia <rafael.garcia@clever.com>
2017-09-29 16:37:10 +0000

    update handler code

--- handler.go
+++ handler.go
@@ -10,7 +10,6 @@ import (
 	"github.com/Clever/workflow-manager/gen-go/models"
 	"github.com/Clever/workflow-manager/resources"
 	"github.com/Clever/workflow-manager/store"
-	"github.com/go-openapi/strfmt"
 )
 
 const (
@@ -39,7 +38,7 @@ func (h Handler) HealthCheck(ctx context.Context) error {
 // NewWorkflowDefinition creates a new workflow
 func (h Handler) NewWorkflowDefinition(ctx context.Context, workflowReq *models.NewWorkflowDefinitionRequest) (*models.WorkflowDefinition, error) {
 	//TODO: validate states
-	if len(workflowReq.States) == 0 {
+	if len(workflowReq.StateMachine.States) == 0 {
 		return &models.WorkflowDefinition{}, fmt.Errorf("Must define at least one state")
 	}
 	if workflowReq.Name == "" {
@@ -51,11 +50,11 @@ func (h Handler) NewWorkflowDefinition(ctx context.Context, workflowReq *models.
 		return &models.WorkflowDefinition{}, err
 	}
 
-	if err := h.store.SaveWorkflowDefinition(workflow); err != nil {
+	if err := h.store.SaveWorkflowDefinition(*workflow); err != nil {
 		return &models.WorkflowDefinition{}, err
 	}
 
-	return apiWorkflowDefinitionFromStore(workflow), nil
+	return workflow, nil
 }
 
 // UpdateWorkflowDefinition creates a new version for an existing workflow
@@ -64,7 +63,7 @@ func (h Handler) UpdateWorkflowDefinition(ctx context.Context, input *models.Upd
 	if workflowReq == nil || workflowReq.Name != input.Name {
 		return &models.WorkflowDefinition{}, fmt.Errorf("Name in path must match WorkflowDefinition object")
 	}
-	if len(workflowReq.States) == 0 {
+	if len(workflowReq.StateMachine.States) == 0 {
 		return &models.WorkflowDefinition{}, fmt.Errorf("Must define at least one state")
 	}
 
@@ -73,27 +72,17 @@ func (h Handler) UpdateWorkflowDefinition(ctx context.Context, input *models.Upd
 		return &models.WorkflowDefinition{}, err
 	}
 
-	workflow, err = h.store.UpdateWorkflowDefinition(workflow)
+	updatedWorkflow, err := h.store.UpdateWorkflowDefinition(*workflow)
 	if err != nil {
 		return &models.WorkflowDefinition{}, err
 	}
 
-	return apiWorkflowDefinitionFromStore(workflow), nil
+	return &updatedWorkflow, nil
 }
 
 // GetWorkflowDefinitions retrieves a list of the latest version of each workflow
 func (h Handler) GetWorkflowDefinitions(ctx context.Context) ([]models.WorkflowDefinition, error) {
-	workflows, err := h.store.GetWorkflowDefinitions()
-
-	if err != nil {
-		return []models.WorkflowDefinition{}, err
-	}
-	apiWorkflowDefinitions := []models.WorkflowDefinition{}
-	for _, workflow := range workflows {
-		apiWorkflowDefinition := apiWorkflowDefinitionFromStore(workflow)
-		apiWorkflowDefinitions = append(apiWorkflowDefinitions, *apiWorkflowDefinition)
-	}
-	return apiWorkflowDefinitions, nil
+	return h.store.GetWorkflowDefinitions()
 }
 
 // GetWorkflowDefinitionVersionsByName fetches either:
@@ -105,39 +94,27 @@ func (h Handler) GetWorkflowDefinitionVersionsByName(ctx context.Context, input 
 		if err != nil {
 			return []models.WorkflowDefinition{}, err
 		}
-		return []models.WorkflowDefinition{*(apiWorkflowDefinitionFromStore(workflow))}, nil
+		return []models.WorkflowDefinition{workflow}, nil
 	}
-
-	apiWorkflowDefinitions := []models.WorkflowDefinition{}
-	workflows, err := h.store.GetWorkflowDefinitionVersions(input.Name)
-	if err != nil {
-		return []models.WorkflowDefinition{}, err
-	}
-	for _, workflow := range workflows {
-		apiWorkflowDefinition := apiWorkflowDefinitionFromStore(workflow)
-		apiWorkflowDefinitions = append(apiWorkflowDefinitions, *apiWorkflowDefinition)
-	}
-
-	return apiWorkflowDefinitions, nil
+	return h.store.GetWorkflowDefinitionVersions(input.Name)
 }
 
 // GetWorkflowDefinitionByNameAndVersion allows fetching an existing WorkflowDefinition by providing it's name and version
 func (h Handler) GetWorkflowDefinitionByNameAndVersion(ctx context.Context, input *models.GetWorkflowDefinitionByNameAndVersionInput) (*models.WorkflowDefinition, error) {
-	workflow, err := h.store.GetWorkflowDefinition(input.Name, int(input.Version))
+	wfd, err := h.store.GetWorkflowDefinition(input.Name, int(input.Version))
 	if err != nil {
-		return &models.WorkflowDefinition{}, err
+		return nil, err
 	}
-	return apiWorkflowDefinitionFromStore(workflow), nil
+	return &wfd, nil
 }
 
 // PostStateResource creates a new state resource
 func (h Handler) PostStateResource(ctx context.Context, i *models.NewStateResource) (*models.StateResource, error) {
 	stateResource := resources.NewBatchResource(i.Name, i.Namespace, i.URI)
-	if err := h.store.SaveStateResource(stateResource); err != nil {
+	if err := h.store.SaveStateResource(*stateResource); err != nil {
 		return &models.StateResource{}, err
 	}
-
-	return apiStateResourceFromStore(stateResource), nil
+	return stateResource, nil
 }
 
 // PutStateResource creates or updates a state resource
@@ -155,11 +132,11 @@ func (h Handler) PutStateResource(ctx context.Context, i *models.PutStateResourc
 
 	stateResource := resources.NewBatchResource(
 		i.NewStateResource.Name, i.NewStateResource.Namespace, i.NewStateResource.URI)
-	if err := h.store.SaveStateResource(stateResource); err != nil {
+	if err := h.store.SaveStateResource(*stateResource); err != nil {
 		return &models.StateResource{}, err
 	}
 
-	return apiStateResourceFromStore(stateResource), nil
+	return stateResource, nil
 }
 
 // GetStateResource fetches a StateResource given a name and namespace
@@ -168,8 +145,7 @@ func (h Handler) GetStateResource(ctx context.Context, i *models.GetStateResourc
 	if err != nil {
 		return &models.StateResource{}, err
 	}
-
-	return apiStateResourceFromStore(stateResource), nil
+	return &stateResource, nil
 }
 
 // DeleteStateResource removes a StateResource given a name and namespace
@@ -179,7 +155,7 @@ func (h Handler) DeleteStateResource(ctx context.Context, i *models.DeleteStateR
 
 // StartWorkflow starts a new Workflow for the given WorkflowDefinition
 func (h Handler) StartWorkflow(ctx context.Context, req *models.StartWorkflowRequest) (*models.Workflow, error) {
-	var workflowDefinition resources.WorkflowDefinition
+	var workflowDefinition models.WorkflowDefinition
 	var err error
 	if req.WorkflowDefinition.Version < 0 {
 		workflowDefinition, err = h.store.LatestWorkflowDefinition(req.WorkflowDefinition.Name)
@@ -193,19 +169,17 @@ func (h Handler) StartWorkflow(ctx context.Context, req *models.StartWorkflowReq
 	if req.Queue == nil {
 		return &models.Workflow{}, fmt.Errorf("workflow queue cannot be nil")
 	}
-	// convert request's tags (map[string]interface{}) to format for store (map[string]string)
-	storeTags, err := storeTagsFromAPI(req.Tags)
+	// verify request's tags (map[string]interface{}) are actually map[string]string
+	if err := validateTagsMap(req.Tags); err != nil {
+		return &models.Workflow{}, err
+	}
+
+	workflow, err := h.manager.CreateWorkflow(workflowDefinition, req.Input, req.Namespace, *req.Queue, req.Tags)
 	if err != nil {
 		return &models.Workflow{}, err
 	}
 
-	workflow, err := h.manager.CreateWorkflow(workflowDefinition, req.Input, req.Namespace, *req.Queue, storeTags)
-
-	if err != nil {
-		return &models.Workflow{}, err
-	}
-
-	return apiWorkflowFromStore(*workflow), nil
+	return workflow, nil
 }
 
 // GetWorkflows returns a summary of all workflows matching the given query.
@@ -241,7 +215,7 @@ func (h Handler) GetWorkflows(
 	results := []models.Workflow{}
 	for _, workflow := range workflows {
 		h.manager.UpdateWorkflowStatus(&workflow)
-		results = append(results, *apiWorkflowFromStore(workflow))
+		results = append(results, workflow)
 	}
 	return results, nextPageToken, nil
 }
@@ -258,13 +232,13 @@ func (h Handler) GetWorkflowByID(ctx context.Context, workflowID string) (*model
 		return &models.Workflow{}, err
 	}
 
-	return apiWorkflowFromStore(workflow), nil
+	return &workflow, nil
 }
 
 // CancelWorkflow cancels all the jobs currently running or queued for the Workflow and
 // marks the workflow as cancelled
 func (h Handler) CancelWorkflow(ctx context.Context, input *models.CancelWorkflowInput) error {
-	workflow, err := h.store.GetWorkflowByID(input.WorkflowId)
+	workflow, err := h.store.GetWorkflowByID(input.WorkflowID)
 	if err != nil {
 		return err
 	}
@@ -274,128 +248,28 @@ func (h Handler) CancelWorkflow(ctx context.Context, input *models.CancelWorkflo
 
 // TODO: the functions below should probably just be functions on the respective resources.<Struct>
 
-func newWorkflowDefinitionFromRequest(req models.NewWorkflowDefinitionRequest) (resources.WorkflowDefinition, error) {
-	if req.StartAt == "" {
-		return resources.WorkflowDefinition{}, fmt.Errorf("startAt is a required field")
+func newWorkflowDefinitionFromRequest(req models.NewWorkflowDefinitionRequest) (*models.WorkflowDefinition, error) {
+	if req.StateMachine.StartAt == "" {
+		return nil, fmt.Errorf("StartAt is a required field")
 	}
 
-	states := map[string]resources.State{}
-	for _, s := range req.States {
-		// TODO: Task=>Job? (INFRA-2483)
-		if s.Type != "" && s.Type != "Task" {
-			return resources.WorkflowDefinition{}, fmt.Errorf("Only States of `type=Task` are supported")
-		}
-		workerState, err := resources.NewWorkerState(s.Name, s.Next, s.Resource, s.End, s.Retry)
-		if err != nil {
-			return resources.WorkflowDefinition{}, err
-		}
-		states[workerState.Name()] = workerState
-	}
-
-	if _, ok := states[req.StartAt]; !ok {
-		return resources.WorkflowDefinition{}, fmt.Errorf("startAt state %s not defined", req.StartAt)
-	}
-
-	// fill in dependencies for states
-	for _, s := range states {
-		if !s.IsEnd() {
-			if _, ok := states[s.Next()]; !ok {
-				return resources.WorkflowDefinition{}, fmt.Errorf("%s.Next=%s, but %s not defined",
-					s.Name(), s.Next(), s.Next())
-			}
-			states[s.Next()].AddDependency(s)
+	for _, s := range req.StateMachine.States {
+		if s.Type != "" && s.Type != models.SLStateTypeTask {
+			return nil, fmt.Errorf("Only States of `type=Task` are supported")
 		}
 	}
 
-	wfd, err := resources.NewWorkflowDefinition(req.Name, req.Description, req.StartAt, states)
-	if err != nil {
-		return wfd, err
+	if _, ok := req.StateMachine.States[req.StateMachine.StartAt]; !ok {
+		return nil, fmt.Errorf("StartAt state %s not defined", req.StateMachine.StartAt)
 	}
-	wfd.Manager = req.Manager
-	return wfd, nil
+	return resources.NewWorkflowDefinition(req.Name, req.StateMachine)
 }
 
-func apiWorkflowDefinitionFromStore(wf resources.WorkflowDefinition) *models.WorkflowDefinition {
-	states := []*models.State{}
-	for _, s := range wf.OrderedStates() {
-		states = append(states, &models.State{
-			Resource: s.Resource(),
-			Name:     s.Name(),
-			Next:     s.Next(),
-			End:      s.IsEnd(),
-			Type:     s.Type(),
-			Retry:    s.Retry(),
-		})
-	}
-
-	return &models.WorkflowDefinition{
-		Name:      wf.Name(),
-		Version:   int64(wf.Version()),
-		StartAt:   wf.StartAt().Name(),
-		CreatedAt: strfmt.DateTime(wf.CreatedAt()),
-		States:    states,
-		Manager:   wf.Manager,
-	}
-}
-
-func apiWorkflowFromStore(workflow resources.Workflow) *models.Workflow {
-	jobs := []*models.Job{}
-	for _, job := range workflow.Jobs {
-		jobs = append(jobs, &models.Job{
-			ID:           job.ID,
-			Attempts:     job.Attempts,
-			Container:    job.ContainerId,
-			CreatedAt:    strfmt.DateTime(job.CreatedAt),
-			Input:        job.Input,
-			Output:       job.Output,
-			StartedAt:    strfmt.DateTime(job.StartedAt),
-			State:        job.State,
-			Status:       string(job.Status),
-			StatusReason: job.StatusReason,
-			StoppedAt:    strfmt.DateTime(job.StoppedAt),
-		})
-	}
-
-	return &models.Workflow{
-		ID:                 workflow.ID,
-		CreatedAt:          strfmt.DateTime(workflow.CreatedAt),
-		LastUpdated:        strfmt.DateTime(workflow.LastUpdated),
-		Jobs:               jobs,
-		WorkflowDefinition: apiWorkflowDefinitionFromStore(workflow.WorkflowDefinition),
-		Status:             string(workflow.Status),
-		Namespace:          workflow.Namespace,
-		Queue:              workflow.Queue,
-		Input:              workflow.Input,
-		Tags:               apiTagsFromStore(workflow.Tags),
-	}
-}
-
-func apiTagsFromStore(storeTags map[string]string) map[string]interface{} {
-	apiTags := make(map[string]interface{})
-	for key, val := range storeTags {
-		apiTags[key] = val
-	}
-	return apiTags
-}
-
-func storeTagsFromAPI(apiTags map[string]interface{}) (map[string]string, error) {
-	storeTags := make(map[string]string)
-	for key, val := range apiTags {
-		valueString, ok := val.(string)
-		if !ok {
-			return nil, fmt.Errorf("error converting value to string: %+v", val)
+func validateTagsMap(apiTags map[string]interface{}) error {
+	for _, val := range apiTags {
+		if _, ok := val.(string); !ok {
+			return fmt.Errorf("error converting tag value to string: %+v", val)
 		}
-		storeTags[key] = valueString
 	}
-	return storeTags, nil
-}
-
-func apiStateResourceFromStore(stateResource resources.StateResource) *models.StateResource {
-	return &models.StateResource{
-		Name:        stateResource.Name,
-		Namespace:   stateResource.Namespace,
-		URI:         stateResource.URI,
-		LastUpdated: strfmt.DateTime(stateResource.LastUpdated),
-		Type:        stateResource.Type,
-	}
+	return nil
 }
--- handler_test.go
+++ handler_test.go
@@ -10,59 +10,37 @@ import (
 // TestNewWorkflowDefinitionFromRequest tests the newWorkflowFromRequest helper
 func TestNewWorkflowDefinitionFromRequest(t *testing.T) {
 	workflowReq := models.NewWorkflowDefinitionRequest{
-		Name:    "test-workflow",
-		StartAt: "start-state",
-		States: []*models.State{
-			&models.State{
-				Name:     "start-state",
-				Type:     "Task",
-				Next:     "second-state",
-				Resource: "test-resource",
-			},
-			&models.State{
-				Name:     "second-state",
-				Type:     "Task",
-				Next:     "end-state",
-				Resource: "test-resource-2",
-			},
-			&models.State{
-				Name:     "end-state",
-				Type:     "Task",
-				Resource: "test-resource-3",
-				End:      true,
+		Name: "test-workflow",
+		StateMachine: &models.SLStateMachine{
+			StartAt: "start-state",
+			States: map[string]models.SLState{
+				"start-state": models.SLState{
+					Type:     models.SLStateTypeTask,
+					Next:     "second-state",
+					Resource: "test-resource",
+				},
+				"second-state": models.SLState{
+					Type:     models.SLStateTypeTask,
+					Next:     "end-state",
+					Resource: "test-resource-2",
+				},
+				"end-state": models.SLState{
+					Type:     models.SLStateTypeTask,
+					Resource: "test-resource-3",
+					End:      true,
+				},
 			},
 		},
 	}
 
-	wf, err := newWorkflowDefinitionFromRequest(workflowReq)
+	_, err := newWorkflowDefinitionFromRequest(workflowReq)
 	t.Log("No error converting from new workflow request to resource")
 	assert.Nil(t, err)
-
-	// We currently only support linear workflows (single dependency)
-	// and we also require that start-state does not have dependencies.
-	// i.e. start states in the middle of the workflow are not allowed.
-	for _, s := range wf.States() {
-		if s.Name() == wf.StartAt().Name() {
-			t.Logf("Starting state has no dependencies")
-			assert.Empty(t, s.Dependencies())
-		} else {
-			t.Logf("State %s has the correct infered dependency", s.Name())
-			depName := s.Dependencies()[0]
-			assert.Equal(t, wf.States()[depName].Next(), s.Name())
-		}
-	}
 }
 
-// TestStoreTagsFromAPI tests the storeTagsFromAPI helper
-func TestStoreTagsFromAPI(t *testing.T) {
-	// test with valid (string-string) tags
+func TestValidateTagsMap(t *testing.T) {
 	apiTags := map[string]interface{}{"team": "infra", "k": "v"}
-	expected1 := map[string]string{"team": "infra", "k": "v"}
-	tags1, err := storeTagsFromAPI(apiTags)
-	assert.Nil(t, err)
-	assert.Equal(t, tags1, expected1)
-	// test with an invalid tag - value must be string
+	assert.Nil(t, validateTagsMap(apiTags))
 	apiTags["num"] = 4
-	_, err = storeTagsFromAPI(apiTags)
-	assert.Error(t, err, "error converting value to string: 4")
+	assert.Error(t, validateTagsMap(apiTags))
 }
