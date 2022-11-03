6ff07fc48e855b622cd2dd4010f42cf2a89705e8
Chris Martin <101210272+ChrisScotMartin@users.noreply.github.com>
2022-11-03 17:21:05 -0400

    docs.go was being ignored despite added with git add -f

---
+++ mocks/docs.go
@@ -0,0 +1,4 @@
+// Package mocks holds generated code for mocks of dependencies, for testing.
+// We don't commit the atual mocks, but committing an empty package helps bootstrap setting up dependencies
+// (i.e. without this file, go mod vendor fails on a fresh clone since tests import this package)
+package mocks
