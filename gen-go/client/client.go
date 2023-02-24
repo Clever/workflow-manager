4ef6f9d5917119ad643ffa00803e950a55b28e45
nbhatia823 <nikhil.bhatia@clever.com>
2023-02-24 12:50:45 -0800

    version

--- gen-go/client/client.go
+++ gen-go/client/client.go
@@ -26,7 +26,7 @@ var _ = strconv.FormatInt
 var _ = bytes.Compare
 
 // Version of the client.
-const Version = "0.15.0"
+const Version = "0.15.1"
 
 // VersionHeader is sent with every request.
 const VersionHeader = "X-Client-Version"
