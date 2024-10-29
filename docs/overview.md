9d1ecf80a7a94ba3b6a9b4469cece55cac273d7f
Jake Gutierrez <jake.gutierrez@clever.com>
2024-10-29 11:45:45 -0500

    bump version

--- docs/overview.md
+++ docs/overview.md
@@ -7,7 +7,7 @@ Orchestrator for AWS Step Functions
 
 
 ### Version information
-*Version* : 0.16.1
+*Version* : 0.16.2
 
 
 ### URI scheme
--- gen-js/index.js
+++ gen-js/index.js
@@ -2695,7 +2695,7 @@ module.exports.Errors = Errors;
 
 module.exports.DefaultCircuitOptions = defaultCircuitOptions;
 
-const version = "0.16.1";
+const version = "0.16.2";
 const versionHeader = "X-Client-Version";
 module.exports.Version = version;
 module.exports.VersionHeader = versionHeader;
